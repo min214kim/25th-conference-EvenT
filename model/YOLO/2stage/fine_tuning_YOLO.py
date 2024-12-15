815ac39f6188632e065f147dbbb36b3e856a3ef6
root <wjddid000624@yonsei.ac.kr>
2024-12-15 11:51:46 +0000

    chore: change directory

--- model/YOLO/2stage/fine_tuning_YOLO.py
+++ model/YOLO/2stage/fine_tuning_YOLO.py
@@ -3,13 +3,13 @@ from ultralytics import YOLO
 
 
 def train_yolo():
-    model = YOLO("yolo11n.pt")
+    model = YOLO("yolo11m.pt")
 
-    result_model = model.train(data="data.yaml", imgsz = 640, epochs = 100, device = [0, 1])
+    result_model = model.train(data="/dataset/after_processing/split_data/data.yaml", imgsz = 640, epochs = 100, device = [0, 1], workers = 12)
 
-    result_model.save("yolo11n_fine_tuned.pt")
+    result_model.save("yolo11m_fine_tuned.pt")
 
-    results = model("")
+    results = model("/dataset/after_processing/split_data/val/images/1263525.jpg")
 
     print("Fine-tuning is done!")
     print(results.show())
