4f5be24a1d6649c54a653cdb48543cdec66f9cdf
root <wjddid000624@yonsei.ac.kr>
2024-12-24 08:37:01 +0000

    feat: apply models to input images

--- model/model.py
+++ model/model.py
@@ -41,3 +41,23 @@ class EfficientNetMultiTask(nn.Module):
         task = unicodedata.normalize('NFC', task)
         features = self.base_model(x)  # EfficientNetB7 feature extractor
         return self.task_heads[task](features)
+    
+class EmbeddingModel(nn.Module):
+    def __init__(self):
+        super(EmbeddingModel, self).__init__()
+        embedding_dim = 128
+        self.model = AutoModel.from_pretrained('Marqo/marqo-fashionSigLIP', trust_remote_code=True)
+        self.processor = AutoProcessor.from_pretrained('Marqo/marqo-fashionSigLIP', trust_remote_code=True)
+
+        self.embeddingLayer1 = nn.Linear(768, 256)
+        self.embeddingLayer2 = nn.Linear(256, embedding_dim)
+
+    def forward(self, x):
+        processed = self.processor(images=x, padding='max_length', return_tensors="pt")
+        pixel_values = processed['pixel_values'].to(next(self.model.parameters()).device)
+        x = self.model.get_image_features(pixel_values, normalize=True)
+        x = x.clone().detach().requires_grad_(True)
+
+        embedding = self.embeddingLayer1(x)
+        embedding = self.embeddingLayer2(embedding)
+        return embedding
